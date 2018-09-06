c872be324a29bd370f1f6c7f9060257258e60c26
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-06 12:18:14 -0700

    Add files via upload

---
+++ numerictobinary.py
@@ -0,0 +1,115 @@
+#==================================================================
+#			            Numeric to Binary 
+#==================================================================
+
+''' 
+Date    : Thu 06 Sep 2018 12:03:36 PM PDT 
+Subject : Python
+Title   : Numeric to Binary 
+Source  : Practice
+''' 
+
+#
+
+'''
+NOTES:
+
+'''
+
+remain = ''
+binary = ''
+num = int(input('Please enter a number: '))
+
+if num  > 127:
+	remain = num - 128
+	if remain == 0:
+		print('that it')
+		
+		
+print(remain)
+print(binary)
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

47862c78189e48c5578ab82334a7e2b4987f2be1
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-08 16:02:38 -0700

    Add files via upload

--- practicewithframes.py
+++ practicewithframes.py
@@ -19,22 +19,35 @@ NOTES:
 #
 
 from tkinter import*
-import time
+import time;
 import random
 import datetime
 
-crm = Tk()
-crm.geometry('800x800')
-crm.title('Customer Relations Management System
+CRM = Tk()
+CRM.geometry('800x800+0+0')
+CRM.title('Customer Relations Management System')
 
+frame1= Frame(CRM, width = 800, height = 40, bd = 2, relief='raise')
+frame1.pack(side=TOP)
 
+frame1_title=Label(frame1, font=('arial', 5, 'bold'), text='Customer Service Relations Manager', bd=4, anchor='w')
+frame1_title.grid(row=0, column=0)
 
+frame2= Frame(CRM, width = 800, height = 40, bd = 2, relief='raise')
+frame2.pack(side=TOP)
 
+text_Input=StringVar()
+txtDisplay = Entry(frame2, font=('arial', 20, 'bold'), textvariable=text_Input, bd=8, insertwidth=6, justify='right')
+txtDisplay.grid(columnspan=4)
 
+# why does the text sixe make the entire frame smaller 
 
+'''
+frame2a= Frame(frame2, width = 40, height = 20, bd = 2, relief='raise')
+frame2a.pack(side=TOP)
+'''
 
-
-
+CRM.mainloop()
 
 
 
--- virtualcashregister.py
+++ virtualcashregister.py
@@ -1,12 +1,12 @@
 #==================================================================
-#			             
+#			             Virtual cash register
 #==================================================================
 
 ''' 
 Date    : Tue 04 Sep 2018 10:46:15 PM PDT 
-Subject : 
-Title   : 
-Source  : 
+Subject : python
+Title   : virtual cash register
+Source  : https://www.youtube.com/watch?v=sHa827pDvnw&t=70s
 ''' 
 
 #
@@ -17,6 +17,55 @@ NOTES:
 '''
 
 # 
+
+from tkinter import*
+import time;
+import random 
+import datetime
+
+root = Tk()
+root.geometry('1350x650+0+0')
+root.title('Billing System')
+
+Tops = Frame(root, width=1350, height=100, bd=8, relief='raise')
+Tops.pack(side=TOP)
+
+f1 = Frame(root, width=900, height=650, bd=8, relief='raise')
+f1.pack(side=LEFT)
+f2 = Frame(root, width=440, height=650, bd=8, relief='raise')
+f2.pack(side=LEFT)
+
+f1a = Frame(f1, width=900, height=330, bd=8, relief='raise')
+f1a.pack(side=TOP)
+f2a = Frame(f1, width=900, height=320, bd=8, relief='raise')
+f2a.pack(side=BOTTOM)
+
+f1aa = Frame(f1a, width=400, height=430, bd=8, relief='raise')
+f1aa.pack(side=LEFT)
+f1ab = Frame(f1a, width=400, height=430, bd=8, relief='raise')
+f1ab.pack(side=RIGHT)
+
+
+f2aa = Frame(f2a, width=450, height=330, bd=8, relief='raise')
+f2aa.pack(side=LEFT)
+f2ab = Frame(f2a, width=450, height=330, bd=8, relief='raise')
+f2ab.pack(side=LEFT)
+
+lblInfo=Label(Tops, font=('arial', 60, 'bold'), text='          Online Billing Systems           ', bd=10, anchor='w')
+lblInfo.grid(row=0, column=0)
+
+#==================================================Calculator=================================================#
+text_Input=StringVar()
+
+txtDisplay = Entry(f2, font=('arial', 20, 'bold'), textvariable=text_Input, bd=40, insertwidth=6, justify='right')
+txtDisplay.grid(columnspan=4)
+
+
+
+
+root.mainloop()
+
+
 
 
 

f9f4b17776d1bff2609741d2e3956694aefed595
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-08 23:52:51 -0700

    Add files via upload

---
+++ tenbytengrid.py
@@ -0,0 +1,322 @@
+
+
+from tkinter import*
+
+root = Tk()
+
+'''
+label1 = Label(root, text='Name') 
+label1.grid(row=0, column=0)			
+
+label2 = Label(root, text='Password')	
+label2.grid(row=1, column=0)
+'''
+
+#==================column=0==================#
+entry0_0 = Entry(root, width=3)				
+entry0_0.grid(row=0, column=0)
+entry1_0 = Entry(root, width=3)
+entry1_0.grid(row=1, column=0)
+entry2_0 = Entry(root, width=3)
+entry2_0.grid(row=2, column=0)
+entry3_0 = Entry(root, width=3)				
+entry3_0.grid(row=3, column=0)
+entry4_0 = Entry(root, width=3)
+entry4_0.grid(row=4, column=0)
+entry5_0 = Entry(root, width=3)
+entry5_0.grid(row=5, column=0)
+entry6_0 = Entry(root, width=3)				
+entry6_0.grid(row=6, column=0)
+entry7_0 = Entry(root, width=3)
+entry7_0.grid(row=7, column=0)
+entry8_0 = Entry(root, width=3)
+entry8_0.grid(row=8, column=0)
+entry9_0 = Entry(root, width=3)				
+entry9_0.grid(row=9, column=0)
+#==================Columns=1===============#
+entry0_1 = Entry(root, width=3)				
+entry0_1.grid(row=0, column=1)		
+entry1_1 = Entry(root, width=3)
+entry1_1.grid(row=1, column=1)
+entry2_1 = Entry(root, width=3)
+entry2_1.grid(row=2, column=1)
+entry3_1 = Entry(root, width=3)				
+entry3_1.grid(row=3, column=1)		
+entry4_1 = Entry(root, width=3)
+entry4_1.grid(row=4, column=1)
+entry5_1 = Entry(root, width=3)
+entry5_1.grid(row=5, column=1)
+entry6_1 = Entry(root, width=3)
+entry6_1.grid(row=6, column=1)
+entry7_1 = Entry(root, width=3)
+entry7_1.grid(row=7, column=1)
+entry8_1 = Entry(root, width=3)				
+entry8_1.grid(row=8, column=1)		
+entry9_1 = Entry(root, width=3)
+entry9_1.grid(row=9, column=1)
+#==================Columns=2===============#
+entry0_2 = Entry(root, width=3)			
+entry0_2.grid(row=0, column=2)		
+entry1_2 = Entry(root, width=3)
+entry1_2.grid(row=1, column=2)
+entry2_2 = Entry(root, width=3)
+entry2_2.grid(row=2, column=2)
+entry3_2 = Entry(root, width=3)				
+entry3_2.grid(row=3, column=2)		
+entry4_2 = Entry(root, width=3)
+entry4_2.grid(row=4, column=2)
+entry5_2 = Entry(root, width=3)
+entry5_2.grid(row=5, column=2)
+entry6_2 = Entry(root, width=3)
+entry6_2.grid(row=6, column=2)
+entry7_2 = Entry(root, width=3)				
+entry7_2.grid(row=7, column=2)		
+entry8_2 = Entry(root, width=3)
+entry8_2.grid(row=8, column=2)
+entry9_2 = Entry(root, width=3)
+entry9_2.grid(row=9, column=2)
+#==================Columns=3===============#
+entry0_3 = Entry(root, width=3)			
+entry0_3.grid(row=0, column=3)		
+entry1_3 = Entry(root, width=3)
+entry1_3.grid(row=1, column=3)
+entry2_3 = Entry(root, width=3)
+entry2_3.grid(row=2, column=3)
+entry3_3 = Entry(root, width=3)				
+entry3_3.grid(row=3, column=3)		
+entry4_3 = Entry(root, width=3)
+entry4_3.grid(row=4, column=3)
+entry5_3 = Entry(root, width=3)
+entry5_3.grid(row=5, column=3)
+entry6_3 = Entry(root, width=3)
+entry6_3.grid(row=6, column=3)
+entry7_3 = Entry(root, width=3)				
+entry7_3.grid(row=7, column=3)		
+entry8_3 = Entry(root, width=3)
+entry8_3.grid(row=8, column=3)
+entry9_3 = Entry(root, width=3)
+entry9_3.grid(row=9, column=3)
+#==================Columns=4===============#
+entry0_4 = Entry(root, width=3)			
+entry0_4.grid(row=0, column=4)		
+entry1_4 = Entry(root, width=3)
+entry1_4.grid(row=1, column=4)
+entry2_4 = Entry(root, width=3)
+entry2_4.grid(row=2, column=4)
+entry3_4 = Entry(root, width=3)				
+entry3_4.grid(row=3, column=4)		
+entry4_4 = Entry(root, width=3)
+entry4_4.grid(row=4, column=4)
+entry5_4 = Entry(root, width=3)
+entry5_4.grid(row=5, column=4)
+entry6_4 = Entry(root, width=3)
+entry6_4.grid(row=6, column=4)
+entry7_4 = Entry(root, width=3)				
+entry7_4.grid(row=7, column=4)		
+entry8_4 = Entry(root, width=3)
+entry8_4.grid(row=8, column=4)
+entry9_4 = Entry(root, width=3)
+entry9_4.grid(row=9, column=4)
+#==================column=0==================#
+entry0_5 = Entry(root, width=3)				
+entry0_5.grid(row=0, column=5)
+entry1_5 = Entry(root, width=3)
+entry1_5.grid(row=1, column=5)
+entry2_5 = Entry(root, width=3)
+entry2_5.grid(row=2, column=5)
+entry3_5 = Entry(root, width=3)				
+entry3_5.grid(row=3, column=5)
+entry4_5 = Entry(root, width=3)
+entry4_5.grid(row=4, column=5)
+entry5_5 = Entry(root, width=3)
+entry5_5.grid(row=5, column=5)
+entry6_5 = Entry(root, width=3)				
+entry6_5.grid(row=6, column=5)
+entry7_5 = Entry(root, width=3)
+entry7_5.grid(row=7, column=5)
+entry8_5 = Entry(root, width=3)
+entry8_5.grid(row=8, column=5)
+entry9_5 = Entry(root, width=3)				
+entry9_5.grid(row=9, column=5)
+#==================Columns=1===============#
+entry0_6 = Entry(root, width=3)				
+entry0_6.grid(row=0, column=6)		
+entry1_6 = Entry(root, width=3)
+entry1_6.grid(row=1, column=6)
+entry2_6 = Entry(root, width=3)
+entry2_6.grid(row=2, column=6)
+entry3_6 = Entry(root, width=3)				
+entry3_6.grid(row=3, column=6)		
+entry4_6 = Entry(root, width=3)
+entry4_6.grid(row=4, column=6)
+entry5_6 = Entry(root, width=3)
+entry5_6.grid(row=5, column=6)
+entry6_6 = Entry(root, width=3)
+entry6_6.grid(row=6, column=6)
+entry7_6 = Entry(root, width=3)
+entry7_6.grid(row=7, column=6)
+entry8_6 = Entry(root, width=3)				
+entry8_6.grid(row=8, column=6)		
+entry9_6 = Entry(root, width=3)
+entry9_6.grid(row=9, column=6)
+#==================Columns=2===============#
+entry0_7 = Entry(root, width=3)			
+entry0_7.grid(row=0, column=7)		
+entry1_7 = Entry(root, width=3)
+entry1_7.grid(row=1, column=7)
+entry2_7 = Entry(root, width=3)
+entry2_7.grid(row=2, column=7)
+entry3_7 = Entry(root, width=3)				
+entry3_7.grid(row=3, column=7)		
+entry4_7 = Entry(root, width=3)
+entry4_7.grid(row=4, column=7)
+entry5_7 = Entry(root, width=3)
+entry5_7.grid(row=5, column=7)
+entry6_7 = Entry(root, width=3)
+entry6_7.grid(row=6, column=7)
+entry7_7 = Entry(root, width=3)				
+entry7_7.grid(row=7, column=7)		
+entry8_7 = Entry(root, width=3)
+entry8_7.grid(row=8, column=7)
+entry9_7 = Entry(root, width=3)
+entry9_7.grid(row=9, column=7)
+#==================Columns=3===============#
+entry0_8 = Entry(root, width=3)			
+entry0_8.grid(row=0, column=8)		
+entry1_8 = Entry(root, width=3)
+entry1_8.grid(row=1, column=8)
+entry2_8 = Entry(root, width=3)
+entry2_8.grid(row=2, column=8)
+entry3_8 = Entry(root, width=3)				
+entry3_8.grid(row=3, column=8)		
+entry4_8 = Entry(root, width=3)
+entry4_8.grid(row=4, column=8)
+entry5_8 = Entry(root, width=3)
+entry5_8.grid(row=5, column=8)
+entry6_8 = Entry(root, width=3)
+entry6_8.grid(row=6, column=8)
+entry7_8 = Entry(root, width=3)				
+entry7_8.grid(row=7, column=8)		
+entry8_8 = Entry(root, width=3)
+entry8_8.grid(row=8, column=8)
+entry9_8 = Entry(root, width=3)
+entry9_8.grid(row=9, column=8)
+#==================Columns=4===============#
+entry0_9 = Entry(root, width=3)			
+entry0_9.grid(row=0, column=9)		
+entry1_9 = Entry(root, width=3)
+entry1_9.grid(row=1, column=9)
+entry2_9 = Entry(root, width=3)
+entry2_9.grid(row=2, column=9)
+entry3_9 = Entry(root, width=3)				
+entry3_9.grid(row=3, column=9)		
+entry4_9 = Entry(root, width=3)
+entry4_9.grid(row=4, column=9)
+entry5_9 = Entry(root, width=3)
+entry5_9.grid(row=5, column=9)
+entry6_9 = Entry(root, width=3)
+entry6_9.grid(row=6, column=9)
+entry7_9 = Entry(root, width=3)				
+entry7_9.grid(row=7, column=9)		
+entry8_9 = Entry(root, width=3)
+entry8_9.grid(row=8, column=9)
+entry9_9 = Entry(root, width=3)
+entry9_9.grid(row=9, column=9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

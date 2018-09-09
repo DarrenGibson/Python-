742d5216fff9faf9ca297d5d1c4c43f24305c2a8
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-09-09 11:15:13 -0700

    Add files via upload

---
+++ chessgrid.py
@@ -0,0 +1,244 @@
+
+
+from tkinter import*
+
+root = Tk()
+
+'''
+label1 = Label(root, text='Name') 
+label1.grid(row=0, column=0)			
+
+label2 = Label(root, text='Password')	
+label2.grid(row=1, column=0)
+'''
+
+#==================FILE=A==================#
+
+entryA1 = Entry(root, width=3)
+entryA1.grid(row=1, column=0)
+entryA2 = Entry(root, width=3)
+entryA2.grid(row=2, column=0)
+entryA3 = Entry(root, width=3)				
+entryA3.grid(row=3, column=0)
+entryA4 = Entry(root, width=3)
+entryA4.grid(row=4, column=0)
+entryA5 = Entry(root, width=3)
+entryA5.grid(row=5, column=0)
+entryA6 = Entry(root, width=3)				
+entryA6.grid(row=6, column=0)
+entryA7 = Entry(root, width=3)
+entryA7.grid(row=7, column=0)
+entryA8 = Entry(root, width=3)				
+entryA8.grid(row=8, column=0)
+
+#==================FILE=B===============#		
+
+entryB1 = Entry(root, width=3)
+entryB1.grid(row=1, column=1)
+entryB2 = Entry(root, width=3)
+entryB2.grid(row=2, column=1)
+entryB3 = Entry(root, width=3)				
+entryB3.grid(row=3, column=1)		
+entryB4 = Entry(root, width=3)
+entryB4.grid(row=4, column=1)
+entryB5 = Entry(root, width=3)
+entryB5.grid(row=5, column=1)
+entryB6 = Entry(root, width=3)
+entryB6.grid(row=6, column=1)
+entryB7 = Entry(root, width=3)
+entryB7.grid(row=7, column=1)
+entryB8 = Entry(root, width=3)				
+entryB8.grid(row=8, column=1)
+
+#==================FILE=C===============#
+		
+entryC1 = Entry(root, width=3)
+entryC1.grid(row=1, column=2)
+entryC2 = Entry(root, width=3)
+entryC2.grid(row=2, column=2)
+entryC3 = Entry(root, width=3)				
+entryC3.grid(row=3, column=2)		
+entryC4 = Entry(root, width=3)
+entryC4.grid(row=4, column=2)
+entryC5 = Entry(root, width=3)
+entryC5.grid(row=5, column=2)
+entryC6 = Entry(root, width=3)
+entryC6.grid(row=6, column=2)
+entryC7 = Entry(root, width=3)				
+entryC7.grid(row=7, column=2)		
+entryC8 = Entry(root, width=3)				
+entryC8.grid(row=8, column=2)
+
+#==================FILE=D===============#
+	
+entryD1 = Entry(root, width=3)
+entryD1.grid(row=1, column=3)
+entryD2 = Entry(root, width=3)
+entryD2.grid(row=2, column=3)
+entryD3 = Entry(root, width=3)				
+entryD3.grid(row=3, column=3)		
+entryD4 = Entry(root, width=3)
+entryD4.grid(row=4, column=3)
+entryD5 = Entry(root, width=3)
+entryD5.grid(row=5, column=3)
+entryD6 = Entry(root, width=3)
+entryD6.grid(row=6, column=3)
+entryD7 = Entry(root, width=3)				
+entryD7.grid(row=7, column=3)		
+entryD8 = Entry(root, width=3)				
+entryD8.grid(row=8, column=3)
+
+#==================FILE=E===============#
+	
+entryE1 = Entry(root, width=3)
+entryE1.grid(row=1, column=4)
+entryE2 = Entry(root, width=3)
+entryE2.grid(row=2, column=4)
+entryE3 = Entry(root, width=3)				
+entryE3.grid(row=3, column=4)		
+entryE4 = Entry(root, width=3)
+entryE4.grid(row=4, column=4)
+entryE5 = Entry(root, width=3)
+entryE5.grid(row=5, column=4)
+entryE6 = Entry(root, width=3)
+entryE6.grid(row=6, column=4)
+entryE7 = Entry(root, width=3)				
+entryE7.grid(row=7, column=4)		
+entryE8 = Entry(root, width=3)			
+entryE8.grid(row=8, column=4)	
+
+#==================FILE=F==================#
+
+entryF1 = Entry(root, width=3)
+entryF1.grid(row=1, column=5)
+entryF2 = Entry(root, width=3)
+entryF2.grid(row=2, column=5)
+entryF3 = Entry(root, width=3)				
+entryF3.grid(row=3, column=5)
+entryF4 = Entry(root, width=3)
+entryF4.grid(row=4, column=5)
+entryF5 = Entry(root, width=3)
+entryF5.grid(row=5, column=5)
+entryF6 = Entry(root, width=3)				
+entryF6.grid(row=6, column=5)
+entryF7 = Entry(root, width=3)
+entryF7.grid(row=7, column=5)
+entryF8 = Entry(root, width=3)				
+entryF8.grid(row=8, column=5)
+
+#==================FILE=G===============#
+		
+entryG1 = Entry(root, width=3)
+entryG1.grid(row=1, column=6)
+entryG2 = Entry(root, width=3)
+entryG2.grid(row=2, column=6)
+entryG3 = Entry(root, width=3)				
+entryG3.grid(row=3, column=6)		
+entryG4 = Entry(root, width=3)
+entryG4.grid(row=4, column=6)
+entryG5 = Entry(root, width=3)
+entryG5.grid(row=5, column=6)
+entryG6 = Entry(root, width=3)
+entryG6.grid(row=6, column=6)
+entryG7 = Entry(root, width=3)
+entryG7.grid(row=7, column=6)
+entryG8 = Entry(root, width=3)				
+entryG8.grid(row=8, column=6)
+
+#==================FILE=H===============#
+		
+entryH1 = Entry(root, width=3)
+entryH1.grid(row=1, column=7)
+entryH2 = Entry(root, width=3)
+entryH2.grid(row=2, column=7)
+entryH3 = Entry(root, width=3)				
+entryH3.grid(row=3, column=7)		
+entryH4 = Entry(root, width=3)
+entryH4.grid(row=4, column=7)
+entryH5 = Entry(root, width=3)
+entryH5.grid(row=5, column=7)
+entryH6 = Entry(root, width=3)
+entryH6.grid(row=6, column=7)
+entryH7 = Entry(root, width=3)				
+entryH7.grid(row=7, column=7)		
+entryH8 = Entry(root, width=3)			
+entryH8.grid(row=8, column=7)
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

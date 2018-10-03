349f3ea297de9a4a1882c5a0a8c356556ce67370
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-10-03 15:48:34 -0700

    Add files via upload

--- grid4.py
+++ grid4.py
@@ -22,17 +22,42 @@ root = Tk()
 ##############################   grid 0, 0   ######################################
 frame00 = Frame(root)
 frame00.grid(row=0, column=0)
+labela = Label(frame00, text='Frame 00')
+labela.pack()
+##############################
+
+
+
+
+
 ##############################   grif 1, 0    ######################################
 frame10 = Frame(root)
 frame10.grid(row=1, column=0)
+labelb = Label(frame10, text='Frame 10')
+labelb.pack()
+##############################
+
+
+
+
+
 ##############################   grif 0, 1    ######################################
 frame01 = Frame(root)
 frame01.grid(row=0, column=1)
+labelc = Label(frame01, text='Frame 01')
+labelc.pack()
+##############################
+
+
+
+
+
 ##############################   grif 1, 1    ######################################
 frame11 = Frame(root)
 frame11.grid(row=1, column=1)
-
-
+labeld = Label(frame11, text='Frame 11')
+labeld.pack()
+##############################
 
 
 root.mainloop()
